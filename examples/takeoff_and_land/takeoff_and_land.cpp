8eae914efe7f5493f7c40e07f27c9ae86685dc21
Aditya Chanana <achanana@cs.cmu.edu>
2024-05-19 09:55:18 -0400

    Add pipe logic

--- examples/takeoff_and_land/takeoff_and_land.cpp
+++ examples/takeoff_and_land/takeoff_and_land.cpp
@@ -11,10 +11,13 @@
 #include <future>
 #include <memory>
 #include <thread>
+#include <modal_pipe.h>
 
 using namespace mavsdk;
 using std::chrono::seconds;
 using std::this_thread::sleep_for;
+
+static const string kProcessName = "voxl-tracking";
 
 void usage(const std::string& bin_name)
 {
@@ -24,6 +27,42 @@ void usage(const std::string& bin_name)
               << " For UDP : udp://[bind_host][:bind_port]\n"
               << " For Serial : serial:///path/to/serial/dev[:baudrate]\n"
               << "For example, to connect to the simulator use URL: udp://:14540\n";
+}
+
+
+static void tflite_server_connect_cb(int ch, void *context)
+{
+    printf("Connected to tflite server\n");
+}
+
+static void tflite_server_disconnect_cb(int ch, void *context)
+{
+    fprintf(stderr, "Disonnected from tflite server\n");
+}
+
+static void tflite_server_cb(int ch, char *data, int bytes, void *context)
+{
+    for (int off = 0; off + (int)sizeof(ai_detection_t) <= bytes; off += sizeof(ai_detection_t)) {
+        ai_detection_t *detection = reinterpret_cast<ai_detection_t *>(data + off);
+        printf("class confidence: %f\n", detection->class_confidence);
+    }
+}
+
+void setup_pipe() {
+    int ch = pipe_client_get_next_available_channel();
+    const std::string &input_pipe = "/run/mpa/tflite_data";
+
+    main_running = 1;
+
+    pipe_client_set_connect_cb(ch, tflite_server_connect_cb, nullptr);
+    pipe_client_set_disconnect_cb(ch, tflite_server_disconnect_cb, nullptr);
+    pipe_client_set_simple_helper_cb(ch, tflite_server_cb, nullptr);
+
+    if (pipe_client_open(ch, input_pipe.c_str(), kProcessName.c_str(), EN_PIPE_CLIENT_SIMPLE_HELPER, 10 * sizeof(ai_detection_t)))
+    {
+        fprintf(stderr, "Failed to open pipe: %s\n", input_pipe.c_str());
+        return -1;
+    }
 }
 
 int main(int argc, char** argv)
